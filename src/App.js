42f0f7a309c86c2199dfbc02cee1bd881fbef76e
Mehedi Hassan <rehan51322@gmail.com>
2022-04-16 16:14:40 +0600

    auth added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import Login from "./components/Auth/Login/Login";
-import RequireAuth from "./components/Auth/RequireAuth/RequireAuth";
-import Signup from "./components/Auth/Signup/Signup";
+import RequireAuth from "./components/Auth/RequireAuth";
 import Generator from "./components/Generator/Generator";
 import Home from "./components/Home/Home";
 import Nav from "./components/Nav/Nav";
@@ -14,16 +12,11 @@ function App() {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/home" element={<Home />} />
-                <Route
-                    path="/generator"
-                    element={
-                        <RequireAuth>
-                            <Generator />
-                        </RequireAuth>
-                    }
-                />
-                <Route path="/login" element={<Login />} />
-                <Route path="/signup" element={<Signup />} />
+                <Route path="/generator" element={
+                    <RequireAuth>
+                        <Generator />
+                    </RequireAuth>
+                } />
             </Routes>
         </div>
     );
